8316b7b83818128cbc62cb6a0cef93e7f18faf67
HoanDo <sieunhannhenkutesn@gmail.com>
2020-09-20 17:56:06 +0700

    update dashboard score

--- src/components/dashboard/Navbar.js
+++ src/components/dashboard/Navbar.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import {Image,Media} from 'react-bootstrap'
+import {FontPoppins} from '../../styles/common/index'
 export default function Navbar(){
     return(   
-        <nav class="navbar bg-primary sticky-top flex-md-nowrap">
-            <span class="navbar-brand mb-0 h1">Navbar</span>
+        <nav className="navbar sticky-top d-flex justify-content-end">
+        <div className="mr-3 ttt-pointer navbar-badge">
+            <i className="fas fa-bell customize-badge" >
+            <span class="badge">12</span>
+            </i>
+        </div>
+        
+        <Media className="ttt-pointer">
+            <Image 
+                className="avatar-32" 
+                src="https://lh3.googleusercontent.com/-JM2xsdjz2Bw/AAAAAAAAAAI/AAAAAAAAAAA/DVECr-jVlk4/photo.jpg" 
+            roundedCircle  />
+        </Media>
+        
         </nav>
     )
 }
--- src/components/dashboard/Sidebar.js
+++ src/components/dashboard/Sidebar.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import {Slide} from 'react-reveal'
 export default function Sidebar(){
     return(  
-            <nav id="sidebar" className="p-0 bg-success d-flex justify-content-center align-items-center">
-            123
+        <Slide left>
+            <nav id="sidebar" className="p-0 d-flex justify-content-center align-items-center">
+             <ul className="sidebar-ul-customize">
+                
+                    <li className="ttt-pointer"><i class="lni lni-home"></i></li>
+                    <li className="ttt-pointer"><i class="lni lni-direction-alt"></i></li>
+                    <li className="ttt-pointer"><i class="lni lni-bookmark"></i></li>
+                    <li className="ttt-pointer"><i class="lni lni-cog"></i></li>
+                
+                 
+                
+             </ul>
             </nav>
+        </Slide>
+       
+            
     )
 }
