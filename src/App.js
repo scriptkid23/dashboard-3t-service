96ca3d9b3fc685092cee8b220c55244ad3387cc1
HoanDo <sieunhannhenkutesn@gmail.com>
2020-10-20 11:18:50 +0700

    update app

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import {Route, Switch, Redirect,HashRouter} from "react-router-dom";
 import './styles/App.css'
 import Auth from './layouts/Auth';
 import Dashboard from './layouts/Dashboard';
-
+import Seo from './components/Seo'
 function App() {
   return (
     <HashRouter>
+      <Seo></Seo>
       <Switch>
         <Route path="/auth" render={props => <Auth {...props} />} />
         <Route path="/dashboard" render={props => <Dashboard {...props}/>} />
